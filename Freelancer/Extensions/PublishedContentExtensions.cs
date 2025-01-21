8e014650570edacb7a72896b99a5f34d63db76ae
Fadi Abji <fadi@wexer.se>
2025-01-21 12:11:50 +0100

    Dynamic site name

---
+++ Freelancer/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Freelancer.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
+        {
+            var homePage = publishedContent.GetHomePage();
+            if (homePage == null) { return null; }
+            return homePage.FirstChild<SiteSettings>();
+        }
+    }
+}
