72c1527f099bd4bc99d8cd837587a916be80a506
Fadi Abji <fadi@wexer.se>
2025-01-28 09:59:51 +0100

    Slimsy

--- Freelancer/Program.cs
+++ Freelancer/Program.cs
@@ -1,3 +1,5 @@
+using Slimsy.DependencyInjection;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.CreateUmbracoBuilder()
@@ -5,6 +7,7 @@ builder.CreateUmbracoBuilder()
     .AddWebsite()
     .AddDeliveryApi()
     .AddComposers()
+    .AddSlimsy()
     .Build();
 
 WebApplication app = builder.Build();
